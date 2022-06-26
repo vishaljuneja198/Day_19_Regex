236d01fe20ebbfa44c1f01afad2dffa44ad0cd96
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 16:31:31 +0530

    added uc1 first three name caps

--- src/com/bridgelaz/Regex.java
+++ src/com/bridgelaz/Regex.java
@@ -1,7 +1,20 @@
-package com.bridgelaz;
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.*;
 
 public class Regex {
     public static void main(String[] args) {
-        System.out.println("welcome to day 19 regex program ");
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}$");
+
+        System.out.println("Enter First Name");
+        Scanner userInput = new Scanner(System.in);
+        String firstName = userInput.nextLine();
+
+        Matcher matcher = pattern.matcher(firstName);
+
+        if (matcher.matches() == false) {
+            System.out.println("Entered Name does not follow the convention");
+        }
     }
 }
